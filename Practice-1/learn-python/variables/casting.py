e0e69dcaeeadf649cb6d3cf65df55fc22aeadd8b
Anarys Umbetov <brodiaga.anarys@gmail.com>
2026-01-24 21:34:15 +0500

    started variables from python tutorial

---
+++ Practice-1/learn-python/variables/casting.py
@@ -0,0 +1,14 @@
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+
+t = "John"
+#is the same as
+t = 'John'
+print(t)
+
+print(x, y, z)
+
+a = 4
+A = "Sally"
+#A will not overwrite a
---
+++ Practice-1/learn-python/variables/creating-variable.py
@@ -0,0 +1,11 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+z = 4
+z = "Sally"
+print(z)
+
+print(type(x))
+print(type(y))
