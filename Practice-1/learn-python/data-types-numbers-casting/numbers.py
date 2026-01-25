64fd92d716f7b00ab234c6338b1692a98899bae1
Anarys Umbetov <brodiaga.anarys@gmail.com>
2026-01-25 13:29:21 +0500

    learned another few concepts of python

---
+++ Practice-1/learn-python/data-types-numbers-casting/numbers.py
@@ -0,0 +1,47 @@
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print()
+
+q = 1
+w = 35493285203950
+e = -4243452
+
+print(type(q))
+print(type(w))
+print(type(e))
+
+print()
+
+r = 1.10
+t = 1.0
+y = -35.59
+
+print(type(r))
+print(type(t))
+print(type(y))
+
+print()
+
+u = 35e3
+i = 12E4
+o = -87.7e100
+
+print(type(u))
+print(type(i))
+print(type(o))
+
+print()
+
+p = 3+5j
+a = 5j
+s = -5j
+
+print(type(p))
+print(type(a))
+print(type(s))
