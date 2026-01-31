1710dd46a0151d44d962c2263135b78708554ab7
Anarys Umbetov <brodiaga.anarys@gmail.com>
2026-01-31 22:35:00 +0500

    assignment operators done

---
+++ Practice-2/learn-python/booleans/function-of-zero.py
@@ -0,0 +1,6 @@
+class myClass():
+    def __len__(self):
+        return 0
+
+myObj = myClass()
+print(bool(myObj))
---
+++ Practice-2/learn-python/booleans/function-returns-bool.py
@@ -0,0 +1,12 @@
+def myFunction():
+    return True
+
+print(myFunction())
+
+if (myFunction()):
+    print("YES!")
+else:
+    print("NO!")
+
+x = 200
+print(isinstance(x, int))
