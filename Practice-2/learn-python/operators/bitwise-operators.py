afb308771a51d40307c641555df078feec2fdc83
Anarys Umbetov <brodiaga.anarys@gmail.com>
2026-02-02 18:19:37 +0500

    operators learned

---
+++ Practice-2/learn-python/operators/bitwise-operators.py
@@ -0,0 +1,3 @@
+print(6 & 3) # 6 is 0110 and 3 is 0011 ==========> 2 which is 0010
+print(6 | 3) # 7 is 0111
+print(6 ^ 3) # 5 is 0101 and ^ this is XOR operator
---
+++ Practice-2/learn-python/operators/identity-operators.py
@@ -0,0 +1,14 @@
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+print(x is not y)
+
+j = [1, 2, 3]
+k = [1, 2, 3]
+print(j == k)
+print(j is k)
