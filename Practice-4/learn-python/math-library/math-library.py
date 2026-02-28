8fc69f1e4ef181060522937d6fd3e89754aba4ac
Anarys Umbetov <brodiaga.anarys@gmail.com>
2026-02-28 14:54:11 +0500

    json, math learned

---
+++ Practice-4/learn-python/math-library/math-library.py
@@ -0,0 +1,25 @@
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+z = abs(-7.25)
+print(z)
+
+d = pow(4, 3)
+print(d)
+
+x = math.sqrt(64)
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+t = math.pi
+print(t)
