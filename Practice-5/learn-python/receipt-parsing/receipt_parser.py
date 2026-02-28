d140007f5cf888ba57e211bc29fc7531fc025ebc
Anarys Umbetov <brodiaga.anarys@gmail.com>
2026-02-28 16:34:12 +0500

    started receipt parsing

--- Practice-5/learn-python/receipt-parsing/receipt_parser.py
+++ Practice-5/learn-python/receipt-parsing/receipt_parser.py
@@ -1,1 +1,8 @@
 import re
+f = open("raw.txt", "rt", encoding="utf-8")
+text = f.read()
+
+patternNamePrice = r"\d+\.\n(?P<Name>.*)\n.*\n.*\n.*Стоимость\n(?P<Price>\d+.*)"
+print(re.findall(patternNamePrice, text))
+
+f.close()
